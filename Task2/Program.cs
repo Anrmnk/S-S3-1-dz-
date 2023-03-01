9f002e8cbb3899b64057f7ebafa87b5a7cd5031e
Anrmnk <anrmnk@gmail.com>
2023-03-01 13:44:23 +0300

    Вторая задача работает

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write ("Введите первое число");
+string answer1 = Console.ReadLine ();
+int number1 = Convert.ToInt32 (answer1);
+Console.Write ("Введите второе число");
+string answer2 = Console.ReadLine ();
+int number2 = Convert.ToInt32 (answer2);
+if (number1 > number2)
+{
+    Console.WriteLine ("Большее число" + " " + number1);
+    Console.WriteLine ("Меньшее число" + " " + number2);
+}
+else
+{
+    Console.WriteLine ("Большее число" + " " + number2);
+    Console.WriteLine ("Меньшее число" + " " + number1);
+}
